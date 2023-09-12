4b0462bfdaf47804f9328eb517d3d1d8b0514633
Yicheng Zhang <20211565@tue-login001.cm.cluster>
2023-09-12 19:21:32 +0200

    change directory structure

---
+++ 02_implementation/00_starting_c_program__lstm/include/lstm.h
@@ -0,0 +1,9 @@
+#ifndef _LSTM_H_
+#define _LSTM_H_
+
+extern FP input[100];
+
+
+
+
+#endif//_LSTM_H_
--- 02_implementation/00_starting_c_program__lstm/src/lstm.c
+++ 02_implementation/00_starting_c_program__lstm/src/lstm.c
@@ -1,3 +1,13 @@
+#include "headers.h"
+
+
 
 FP input[100] = {0};
 
+
+int main()
+{
+
+	printf("Functional\n");
+	return 0;
+}
